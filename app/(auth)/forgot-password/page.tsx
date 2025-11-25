b98cf2a97f97ee084f3674b8c18b135530fc01b3
NovaLia-cg <novalia.project@gmail.com>
2025-11-25 15:50:44 +0900

    send-code-page-fix

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -5,8 +5,7 @@ import { Input } from "@/components/ui/Input";
 export default function ForgotPasswordPage() {
 
   return (
-    // 1. 全体ラッパー: 画面全体の背景色はグレー
-    <div className="min-h-screen w-full bg-gray-background flex flex-col">
+    <div className="min-h-screen w-full bg-background sm:bg-gray-background flex flex-col">
 
       {/* ヘッダー */}
       <Header
@@ -15,38 +14,42 @@ export default function ForgotPasswordPage() {
         className="bg-background border-b shadow-sm"
       />
 
-      {/* 3. コンテンツエリアのラッパー*/}
+      {/* コンテンツエリアのラッパー */}
       <div className="flex-1 flex justify-center w-full">
 
-        {/* 4. スマホ枠（カラム）*/}
-        <div className="w-full max-w-sm bg-background border-border shadow-sm flex flex-col">
+        <div className="w-full sm:max-w-sm bg-background sm:border-border sm:shadow-sm flex flex-col">
 
           <main className="flex-1 flex flex-col justify-center px-6 pb-20">
+            
+            {/* 中身の幅固定 */}
+            <div className="w-full max-w-[402px] mx-auto">
 
-            <h2 className="text-base font-bold text-primary-text mb-10">
-              確認コードを送信
-            </h2>
+              <h2 className="text-base font-bold text-primary-text mb-10">
+                確認コードを送信
+              </h2>
 
-            {/* メールアドレス入力欄 */}
-            <div className="grid w-full items-center gap-1.5">
-                        <label
-                          htmlFor="email"
-                          className="text-sm font-medium text-secondary-text"
-                        >
-                          メールアドレス
-                        </label>
-                        <Input
-                          type="email"
-                          id="email"
-                          placeholder="user@example.com"
-                        />
-                      </div>
+              {/* メールアドレス入力欄 */}
+              <div className="grid w-full items-center gap-1.5">
+                <label
+                  htmlFor="email"
+                  className="text-sm font-medium text-secondary-text"
+                >
+                  メールアドレス
+                </label>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="user@example.com"
+                />
+              </div>
 
-            {/* 送信ボタン */}
-            <div className="mt-30">
-              <ActionLargeButton
-                label="送信"
-              />
+              {/* 送信ボタン*/}
+              <div className="mt-10">
+                <ActionLargeButton
+                  label="送信"
+                />
+              </div>
+
             </div>
           </main>
         </div>
