4a77a72d1efd5fa8087f5575d38165564e036de7
JEY HIRANO <mrjr0722@me.com>
2025-12-10 20:14:08 +0900

    選択モード切替時に選択されたIDをリセットする処理を追加

--- components/features/EntryListPage.tsx
+++ components/features/EntryListPage.tsx
@@ -64,6 +64,7 @@ export const EntryListPage = ({
       setIsMode("summarize");
     }
     setIsSummarizeMode(!isSummarizeMode);
+    setSelectedIds([]);
   }
 
   const handleToggleSelection = (id: string) => {
