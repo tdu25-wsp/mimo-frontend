8d1ec9c319735b11c39dc0d641586ee4c1d7af3f
JEY HIRANO <mrjr0722@me.com>
2025-12-19 23:02:06 +0900

    プロキシAPIのベースURLを定義し、Next.jsのリライト設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy/:path*", // フロントエンドからのアクセスパス
+        destination: `${API_BASE_URL}/:path*`, // 実際のAPIのURL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
