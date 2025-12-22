f2b351a654cc169e18851481553fec787e1bb561
JEY HIRANO <mrjr0722@me.com>
2025-12-23 03:37:39 +0900

    fix(entry-edit-sheet): adjust textarea font size for better responsiveness

--- components/features/EntryEditSheet.tsx
+++ components/features/EntryEditSheet.tsx
@@ -84,7 +84,7 @@ export const EntryEditSheet = () => {
 
         <textarea
           ref={textareaRef}
-          className="w-full resize-none outline-none text-2xl p-2 bg-transparent placeholder:text-gray-300"
+          className="w-full resize-none outline-none text-2xl md:text-base p-2 bg-transparent placeholder:text-gray-300"
           placeholder="メモを入力..."
           value={content}
           onChange={(e) => setContent(e.target.value)}
